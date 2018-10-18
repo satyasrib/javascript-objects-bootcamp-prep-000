e24a7e9442d0bbe7decac871767f26285236d1fe
Satyasri Varaganti <satyasrib@gmail.com>
2018-10-18 01:21:56 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,6 @@ var playlist = { artistNames: "songTitles" }
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[ artistName ] = songTitle
 }
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName
+}

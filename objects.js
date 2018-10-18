a2f50bc2d3192e3c5b33193c1a2cf24c7d296ce6
Satyasri Varaganti <satyasrib@gmail.com>
2018-10-18 01:14:18 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,7 @@
+var playlist = {
+  artist: 'song titles'
+}
+function updatePlaylist(playlist, artistName, songTitle) {
+var playlist = { artistName: ["My Bloody Valentine", "Phil Ochs", "Slowdive"] }
+return playlist
+}

3c91bf961db589bafbcf15b4c199bef5a67a4ac8
Satyasri Varaganti <satyasrib@gmail.com>
2018-10-18 01:20:07 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,5 @@
 var playlist = { artistNames: "songTitles" }
 
-function updatePlaylist(playlist, artistNames, songTitle) {
+function updatePlaylist(playlist, artistName, songTitle) {
   playlist[ artistName ] = songTitle
 }
